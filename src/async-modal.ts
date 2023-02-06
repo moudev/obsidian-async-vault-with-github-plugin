1fc30a8b58db454c62c01990a05d40e54bf6584e
moudev <rigobertomauriciomartinez@gmail.com>
2023-02-06 10:47:03 -0600

    feat: add git results container

--- src/async-modal.ts
+++ src/async-modal.ts
@@ -1,4 +1,6 @@
-import { App, Modal } from "obsidian"
+import { App, Modal, FileSystemAdapter } from "obsidian"
+
+import { executeGitCommand } from "./git"
 
 // https://github.com/zsviczian/obsidian-excalidraw-plugin/blob/1.8.12/src/dialogs/Prompt.ts#L18
 class AsyncModal extends Modal {
@@ -18,11 +20,7 @@ class AsyncModal extends Modal {
 		const form = div.createEl("form")
     form.addClass("async-form")
     form.type = "submit"
-    form.onsubmit = (e) => {
-      e.preventDefault()
-			console.log("Submit", messageInput.value)
-    }
-
+    
 		form.createEl("label").setText("Commit message:")
     const messageInput = form.createEl("input")
     messageInput.type = "text"
@@ -36,11 +34,43 @@ class AsyncModal extends Modal {
 		const submitButton = formActions.createEl("button")
 		submitButton.setText("Submit")
     submitButton.addClass("mod-cta")
+
+    const resultsContainer = contentEl.createDiv()
+    resultsContainer.addClass("async-git-results")
+    
+    form.onsubmit = async (e) => {
+      e.preventDefault()
+			console.log("Submit", messageInput.value)
+      // https://forum.obsidian.md/t/how-to-get-vault-absolute-path/22965/6
+			const adapter = this.app.vault.adapter
+			if (adapter instanceof FileSystemAdapter) {
+				const vaultPath = this.app.vault.adapter.getBasePath()
+        const gitResult = await executeGitCommand("status", vaultPath)
+        this.formatResult(gitResult, resultsContainer)
+			}
+    }
   }
 
   onClose() {
     const { contentEl } = this
     contentEl.empty()
+  }
+
+  formatResult(result: string, container: HTMLElement) {
+    container.innerHTML = ""
+  
+    if (!result) {
+      container.textContent = ""
+    }
+
+    const chunks = result.split("\n")
+    console.log(chunks)
+    container.toggleClass("visible", true)
+    chunks.forEach(text => {
+      if (text) {
+        container.createEl("p").setText(text)
+      }
+    })
   }
 }
 
